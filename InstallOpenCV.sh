d1a8be4b99f027ef487eca6db2c3647da8e310c4
Sashimi <sashimi.nt.0625@gmail.com>
2022-09-11 13:15:03 +0900

    Added settings for Jupyter lab

--- InstallOpenCV.sh
+++ InstallOpenCV.sh
@@ -4,13 +4,22 @@ version="4.6.0"
 
 set -e
 
+echo "--------------------------------------"
+echo "** Removing old OpenCV.          [1/5]"
+echo "--------------------------------------"
 apt -y purge *libopencv*
 
+echo "--------------------------------------"
+echo "** Installing required packages. [2/5]"
+echo "--------------------------------------"
 apt-get install -y build-essential cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
 apt-get install -y libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev
 apt-get install -y libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libdc1394-22-dev
 apt-get install -y libv4l-dev v4l-utils qv4l2 v4l2ucp
 
+echo "--------------------------------------"
+echo "** Downloading the source code.  [3/5]"
+echo "--------------------------------------"
 mkdir opencv
 cd opencv
 
@@ -28,6 +37,10 @@ cd opencv
 
 mkdir release
 cd release
+
+echo "--------------------------------------"
+echo "** Building OpenCV.              [4/5]"
+echo "--------------------------------------"
 cmake   -D WITH_CUDA=ON \
         -D WITH_CUDNN=ON \
         -D CUDA_ARCH_BIN=7.2 \
@@ -52,5 +65,8 @@ make -j$(nproc)
 make install
 make clean
 
+echo "--------------------------------------"
+echo "** Cleaning up.                  [5/5]"
+echo "--------------------------------------"
 cd /
 rm -rf oepncv opencv_contrib
--- README.md
+++ README.md
@@ -1,5 +1,19 @@
 # JetsonXavierNX ML Environment
 You can build an environment for artificial intelligence with JetsonXavierNX.
+Running these Dockerfiles will install the following packages:
+- Machine learning library (one of the following)
+    - Tensorflow v1.15.5
+    - Tensorflow v2.9.1
+    - Pytorch v1.13.0
+        - torchvision v0.13.0
+        - torchaudio v0.12.0
+- opencv v4.6.0
+- numpy
+- matplotlib
+- pandas
+- jupyter lab
+- pillow
+
 ## Build
 ```
 bash ./Build.sh -i <IMAGENAME> -u <USERNAME>
@@ -22,4 +36,12 @@ For example:
 ```
 cd JetsonXavierNX-ML-Environment
 bash ./Run.sh -i tensorflow1 -u sashimi -c
+```
+
+## Run Jupyter Lab
+Jupyter lab is installed at build time. If you want to use it, execute the code below to get the password and then rewrite line 960 of "jupyter_lab_config.py".
+```
+$ ipython
+from jupyter_server.auth import passwd
+passwd()
 ```
